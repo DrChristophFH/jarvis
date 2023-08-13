fe288a94318c0ede604b8c14bb8c3ab892e0df6f
Christoph Daxerer <christoph.daxerer@students.fh-hagenberg.at>
2023-08-13 23:12:42 +0200

    Update cellfactory for parameters and avoiding reinstantiations

--- src/main/java/com/hagenberg/jarvis/views/components/CallStackCell.java
+++ src/main/java/com/hagenberg/jarvis/views/components/CallStackCell.java
@@ -1,19 +1,45 @@
 package com.hagenberg.jarvis.views.components;
 
 import com.hagenberg.jarvis.models.entities.CallStackFrame;
-import javafx.collections.FXCollections;
-import javafx.scene.Scene;
+import com.hagenberg.jarvis.models.entities.MethodParameter;
+import com.hagenberg.jarvis.util.SVGManager;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
 import javafx.scene.layout.HBox;
-import javafx.stage.Stage;
 
 import java.util.List;
 
 public class CallStackCell extends ListCell<CallStackFrame> {
+    private final Button expandButton = new Button();
+    {
+        expandButton.getStyleClass().add("icon-button");
+        expandButton.setGraphic(SVGManager.getInstance().getSVG("/icons/expand"));
+    }
+    private final Label methodName = new Label();
+    {
+        methodName.getStyleClass().add("method-name");
+    }
+    private final Label lineNumber = new Label();
+    {
+        lineNumber.getStyleClass().add("line-number");
+    }
+    private final Label className = new Label();
+    {
+        className.getStyleClass().add("class-name");
+    }
+    private final HBox container = new HBox();
+    {
+        container.getStyleClass().add("call-stack-cell-container");
+        container.getChildren().addAll(expandButton, methodName, lineNumber, className);
+    }
 
+    /**
+     * Updates the cell with the given CallStackFrame.
+     * @implNote Avoids the creation of new objects by reusing the same objects.
+     * @param frame the CallStackFrame to be displayed
+     * @param empty whether the cell is empty or not
+     */
     @Override
     protected void updateItem(CallStackFrame frame, boolean empty) {
         super.updateItem(frame, empty);
@@ -22,28 +48,28 @@ public class CallStackCell extends ListCell<CallStackFrame> {
             setText(null);
             setGraphic(null);
         } else {
-            // Displaying class name and method name
-            Label label = new Label(frame.getClassName() + "." + frame.getMethodName());
-
-            // This can be a button or some GUI element to trigger the expansion
-            Button expandButton = new Button("Expand");
-            expandButton.setOnAction(e -> {
-                // Handle the expansion. For simplicity, this will show parameters in a new window.
-                // You can have a more sophisticated handling.
-                showParameters(frame.getParameters());
-            });
-
-            HBox container = new HBox(label, expandButton);
+            methodName.setText(buildMethodSignature(frame));
+            className.setText(frame.getClassName());
+            expandButton.setOnAction(e -> showParameters(frame.getParameters()));
+            lineNumber.setText(": " + frame.getLineNumber());
             setGraphic(container);
         }
     }
 
-    private void showParameters(List<String> parameters) {
-        // This will open up a new dialog to show parameters.
-        // This can be replaced by any custom behavior like inline expansion, etc.
-        ListView<String> parametersView = new ListView<>(FXCollections.observableArrayList(parameters));
-        Stage stage = new Stage();
-        stage.setScene(new Scene(parametersView));
-        stage.show();
+    private String buildMethodSignature(CallStackFrame frame) {
+        StringBuffer sb = new StringBuffer();
+        sb.append(frame.getMethodName()).append("(");
+        for (MethodParameter parameter : frame.getParameters()) {
+            sb.append(parameter.getSimpleType()).append(" ").append(parameter.getName()).append(", ");
+        }
+        if (!frame.getParameters().isEmpty()) {
+            sb.delete(sb.length() - 2, sb.length());
+        }
+        sb.append(")");
+        return sb.toString();
+    }
+
+    private void showParameters(List<MethodParameter> parameters) {
+
     }
 }
