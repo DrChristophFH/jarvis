eeff2ca4d82a66a6de58cbfdb03f33ea020c36a7
Christoph Daxerer <christoph.daxerer@students.fh-hagenberg.at>
2023-08-13 16:44:21 +0200

    Add debug menu

--- src/main/java/com/hagenberg/jarvis/views/MainView.java
+++ src/main/java/com/hagenberg/jarvis/views/MainView.java
@@ -1,15 +1,18 @@
 package com.hagenberg.jarvis.views;
 
+import com.hagenberg.jarvis.util.SVGManager;
 import com.hagenberg.jarvis.views.components.HideableSplitPane;
 import com.hagenberg.jarvis.views.components.AuxiliaryPane;
 import com.hagenberg.jarvis.views.components.WindowMenu;
 import javafx.geometry.Orientation;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.SplitPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.util.Objects;
@@ -46,11 +49,13 @@ public class MainView {
         SplitPane rootSplitPane = new SplitPane();
         rootSplitPane.getStyleClass().add("root-split-pane");
         rootSplitPane.setOrientation(Orientation.VERTICAL);
+        rootSplitPane.setDividerPositions(0.8, 0.2);
         rootPane.setCenter(rootSplitPane);
 
         HideableSplitPane mainSplitPane = new HideableSplitPane();
         mainSplitPane.getStyleClass().add("main-split-pane");
         mainSplitPane.setOrientation(Orientation.HORIZONTAL);
+        mainSplitPane.setDividerPositions(0.2, 0.8);
         rootSplitPane.getItems().add(mainSplitPane);
 
         SplitPane supportSplitPane = new SplitPane();
@@ -67,9 +72,14 @@ public class MainView {
         leftWindowMenu.addWindow(classes, "/icons/classes.svg");
         leftWindowMenu.addWindow(classInfo, "/icons/class-info.svg");
 
+        VBox objectGraphContainer = new VBox();
+        objectGraphContainer.getStyleClass().add("object-graph-container");
         ScrollPane objectGraph = new ScrollPane();
         objectGraph.getStyleClass().add("object-graph");
-        mainSplitPane.addPane(objectGraph);
+        VBox.setVgrow(objectGraph, javafx.scene.layout.Priority.ALWAYS);
+        HBox debugMenu = buildDebugMenu();
+        objectGraphContainer.getChildren().addAll(objectGraph, debugMenu);
+        mainSplitPane.addPane(objectGraphContainer);
 
         HideableSplitPane rightAuxiliaryContainer = new HideableSplitPane();
         AuxiliaryPane visControl = new AuxiliaryPane("Visualization Controls");
@@ -90,6 +100,25 @@ public class MainView {
         stage.show();
     }
 
-    public void hide() {
+    private HBox buildDebugMenu() {
+        HBox debugMenu = new HBox();
+        debugMenu.getStyleClass().add("debug-menu");
+
+        Button stepInto = buildIconButton("Step Into", "/icons/step-into.svg");
+        Button stepOut = buildIconButton("Step Out", "/icons/step-out.svg");
+        Button stepOver = buildIconButton("Step Over", "/icons/step-over.svg");
+        Button resume = buildIconButton("Resume", "/icons/resume.svg");
+
+        debugMenu.getChildren().addAll(stepInto, stepOut, stepOver, resume);
+
+        return debugMenu;
+    }
+
+    private Button buildIconButton(String text, String resourceName) {
+        Button button = new Button();
+        button.getStyleClass().add("text-icon-button");
+        button.setText(text);
+        button.setGraphic(SVGManager.getInstance().getSVG(resourceName));
+        return button;
     }
 }
