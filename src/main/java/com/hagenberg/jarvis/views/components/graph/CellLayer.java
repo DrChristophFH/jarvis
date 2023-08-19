601d485edfbb756e239c28808e62ad08f41499a6
Christoph Daxerer <christoph.daxerer@students.fh-hagenberg.at>
2023-08-19 15:18:33 +0200

    Delete old graph stuff

--- src/main/java/com/hagenberg/jarvis/views/components/graph/CellLayer.java
+++
@@ -1,7 +0,0 @@
-package com.hagenberg.jarvis.views.components.graph;
-
-import javafx.scene.layout.Pane;
-
-public class CellLayer extends Pane {
-
-}
--- src/main/java/com/hagenberg/jarvis/views/components/graph/Layout.java
+++
@@ -1,7 +0,0 @@
-package com.hagenberg.jarvis.views.components.graph;
-
-public abstract class Layout {
-
-    public abstract void execute();
-
-}
