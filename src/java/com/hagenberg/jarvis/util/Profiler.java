93e62b3f2e7df285e2de58a0ed8df7cd222c1cd4
Christoph Daxerer <christoph.daxerer@students.fh-hagenberg.at>
2023-11-13 20:10:54 +0100

    Update profiler ui

--- src/java/com/hagenberg/jarvis/util/Profiler.java
+++ src/java/com/hagenberg/jarvis/util/Profiler.java
@@ -6,18 +6,49 @@ import java.util.SortedMap;
 import java.util.TreeMap;
 
 import imgui.ImGui;
+import imgui.flag.ImGuiTableFlags;
 
 public class Profiler {
+  // moving average of frame duration
+  private static final int MAX_SAMPLES = 100;
+  private static int frameIndex = 0;
+  private static long frameSum = 0;
+  private static long[] frameTimes = new long[MAX_SAMPLES];
+  private static long frameStart = 0;
+  // individual duration counters
   private static Map<String, Long> startTimes = new HashMap<>();
-  private static SortedMap<String, Long> nanoAcc = new TreeMap<>(); 
+  private static SortedMap<String, Long> nanoAcc = new TreeMap<>();
 
   public static void show() {
+    long frameEnd = System.nanoTime();
+    long frameDuration = frameEnd - frameStart;
+
+    long avgFrameDuration = avgFrameDuration(frameDuration);
+
     ImGui.begin("Profiler");
-    for (String key : nanoAcc.keySet()) {
-      ImGui.text("%-20s: %6.4f ms".formatted(key, nanoAcc.get(key) / 1000000.0));
-      nanoAcc.put(key, 0L);
+    ImGui.text("Frame time: %10.4f ms".formatted(avgFrameDuration / 1000000.0));
+    ImGui.text("Frame rate: %10.0f fps".formatted(1000000000.0 / avgFrameDuration));
+
+    int tableFlags = ImGuiTableFlags.RowBg | ImGuiTableFlags.Resizable | ImGuiTableFlags.Reorderable | ImGuiTableFlags.Hideable;
+
+    if (ImGui.beginTable("table", 2, tableFlags)) {
+      ImGui.tableSetupColumn("Name");
+      ImGui.tableSetupColumn("Acc Time");
+
+      for (String key : nanoAcc.keySet()) {
+        ImGui.tableNextRow();
+        ImGui.tableNextColumn();
+        ImGui.text(key);
+        ImGui.tableNextColumn();
+        ImGui.text("%7.4f ms".formatted(nanoAcc.get(key) / 1000000.0));
+        nanoAcc.put(key, 0L);
+      }
+
+      ImGui.endTable();
     }
+
     ImGui.end();
+    frameStart = System.nanoTime();
   }
 
   public static void start(String name) {
@@ -33,5 +64,13 @@ public class Profiler {
     } else {
       nanoAcc.put(name, duration);
     }
+  }
+
+  private static long avgFrameDuration(long newDuration) {
+    frameSum -= frameTimes[frameIndex];
+    frameSum += newDuration;
+    frameTimes[frameIndex] = newDuration;
+    frameIndex = (frameIndex + 1) % MAX_SAMPLES;
+    return frameSum / MAX_SAMPLES;
   }
 }
